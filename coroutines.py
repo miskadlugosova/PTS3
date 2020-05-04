b368b51f09035eae5cd38f96809ab0acfa038e8b
miskadlugosova <miskadlugosova@gmail.com>
2020-05-04 21:28:23 +0200

    functional degree and neighbourhood functions

--- coroutines.py
+++ coroutines.py
@@ -1,53 +1,53 @@
 import asyncio
+import aiohttp
 import requests
 
 
-def get_neighbours(node):
+async def get_neighbours(node, host="http://localhost:"):
     try:
-        neighbours = (requests.get(f'http://localhost:{node}').text.split(","))
-        return neighbours
+        async with aiohttp.ClientSession() as session:
+            async with session.get(host + str(node)) as resp:
+                return list(str(await resp.text()).split(','))
     except:
         return []
 
 
-async def complete_neighbourhood(start):
-    list_of_neighbours = get_neighbours(start)
+async def complete_neighbourhood(start, host="http://localhost:"):
+    list_of_neighbours = await get_neighbours(start)
 
     async def connect(node):
         for neighbour in list_of_neighbours:
             if (neighbour != node):
-                requests.get(f'http://localhost:{node}/new?port={neighbour}')
+                requests.get(f'{host}{node}/new?port={neighbour}')
 
-    tasks = [asyncio.create_task(connect(node) for node in list_of_neighbours)]
+    tasks = [connect(node) for node in list_of_neighbours]
     await asyncio.gather(*tasks)
 
 
 async def climb_degree(start):
-    list_of_neighbours = get_neighbours(start)
-    print("climb_degree")
-    print(list_of_neighbours)
+    list_of_neighbours = await get_neighbours(start)
     my_degree = len(list_of_neighbours)
     degrees_of_neighbours = []
 
     async def count_degree(node):
-        degrees_of_neighbours.append((node, len(get_neighbours(node))))
-        print(node)
-        print(len(get_neighbours(node)))
+        degrees_of_neighbours.append((node, len(await get_neighbours(node))))
 
-    tasks = [asyncio.create_task(count_degree(node) for node in list_of_neighbours)]
+    tasks = [count_degree(node) for node in list_of_neighbours]
     await asyncio.gather(*tasks)
-    degrees_of_neighbours = sorted(degrees_of_neighbours, key=lambda tup: (-tup[1], tup[0]))
 
+
+    degrees_of_neighbours = sorted(degrees_of_neighbours, key=lambda tup: (-tup[1], tup[0]))
     if my_degree > degrees_of_neighbours[0][1] or (
             my_degree == degrees_of_neighbours[0][1] and start < degrees_of_neighbours[0][0]):
         return start
 
-    return degrees_of_neighbours[0][0]
+    return await climb_degree(degrees_of_neighbours[0][0])
 
 
 async def distance4(start):
-    visited = {}
-    answer = {}
+    visited = set()
+    answer = set()
+    list_of_neighbours = await get_neighbours(start)
 
     async def visit(node, distance):
         if node in answer and distance < 4:
@@ -58,9 +58,21 @@ async def distance4(start):
             if (distance == 4):
                 answer.add(node)
             else:
-                list_of_neighbours = get_neighbours(start)
-                tasks = [asyncio.create_task(visit(neigh, distance + 1) for neigh in list_of_neighbours)]
-                await asyncio.gather(*tasks)
+                list_of_neighbours = await get_neighbours(node)
 
-    await visit(start, 0)
+    tasks = [visit(neigh, 1) for neigh in list_of_neighbours]
+    await asyncio.gather(*tasks)
     return answer
+
+
+async def main():
+    x = await climb_degree(8034)
+    print(x)
+    await complete_neighbourhood(8031)
+    y = await climb_degree(8034)
+    print(y)
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
