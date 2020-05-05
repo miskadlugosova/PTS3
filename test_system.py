e87248267a51cc2b88365c66751807ddf62dfc60
miskadlugosova <miskadlugosova@gmail.com>
2020-05-06 01:18:59 +0200

    working threading, bug in variable type

--- test_system.py
+++ test_system.py
@@ -1,9 +1,38 @@
+import asyncio
+import threading
 import unittest
+import time
+import asynctest
+from initialize_nodes import do_stuff
+from coroutines import climb_degree, complete_neighbourhood, distance4
 
 
 # climb noneixsting node, calculate distance from beginning in linear, complete graph, climb from the same node, calculate distance from beginning again
-class TestSystem(unittest.TestCase):
-    pass
+class TestSystem(asynctest.TestCase):
+    def setUp(self):
+        HOST = "localhost"
+        graph_base = 8030
+        graph = {(0, 1), (1, 0), (1, 2), (2, 1), (2, 3), (3, 2), (3, 4), (4, 3), (4, 5), (5, 4)}
+        graph = {(graph_base + x, graph_base + y) for x, y in graph}
+        nodes = {x for y in graph for x in y}
+        self.condition_ready = threading.Condition()
+        self.condition_done = threading.Condition()
+        self.server = threading.Thread(target=do_stuff, args=(
+            HOST, nodes, graph, self.condition_ready, self.condition_done,))
+
+    async def test_climb_distance_in_linear_graph_completing_and_after_climb_distance(self):
+        self.server.start()
+        with self.condition_ready:
+            self.condition_ready.wait()
+        time.sleep(1)
+        self.assertEqual(await climb_degree(8034), 8031)
+        self.assertEqual(await distance4(8030), {'8034'})
+        await complete_neighbourhood(8034)
+        self.assertEqual(await climb_degree(8034), 8033)
+        self.assertEqual(await distance4(8030), set(8034, 8035))
+        with self.condition_done:
+            self.condition_done.notify()
+        self.server.join()
 
 
 if __name__ == '__main__':
