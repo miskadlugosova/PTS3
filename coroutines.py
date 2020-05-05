2bd2dce7cc8a7aed9be2dcc65de399eb432d47e7
miskadlugosova <miskadlugosova@gmail.com>
2020-05-05 11:44:42 +0200

    updates in distance

--- coroutines.py
+++ coroutines.py
@@ -1,6 +1,7 @@
 import asyncio
 import aiohttp
 import requests
+import queue
 
 
 async def get_neighbours(node, host="http://localhost:"):
@@ -14,6 +15,8 @@ async def get_neighbours(node, host="http://localhost:"):
 
 async def complete_neighbourhood(start, host="http://localhost:"):
     list_of_neighbours = await get_neighbours(start)
+    print("list of neig in complete")
+    print(list_of_neighbours)
 
     async def connect(node):
         for neighbour in list_of_neighbours:
@@ -26,10 +29,10 @@ async def complete_neighbourhood(start, host="http://localhost:"):
 
 async def climb_degree(start):
     list_of_neighbours = await get_neighbours(start)
-    print("climbing")
-    print (list_of_neighbours)
     my_degree = len(list_of_neighbours)
     degrees_of_neighbours = []
+    print("list of neigh in climbing")
+    print(list_of_neighbours)
 
     if my_degree == 0:
         return start
@@ -40,9 +43,9 @@ async def climb_degree(start):
     tasks = [count_degree(node) for node in list_of_neighbours]
     await asyncio.gather(*tasks)
 
-
-
     degrees_of_neighbours = sorted(degrees_of_neighbours, key=lambda tup: (-tup[1], tup[0]))
+    print("sorted degrees")
+    print(degrees_of_neighbours)
     if my_degree > degrees_of_neighbours[0][1] or (
             my_degree == degrees_of_neighbours[0][1] and start < degrees_of_neighbours[0][0]):
         return start
@@ -51,27 +54,38 @@ async def climb_degree(start):
 
 
 async def distance4(start):
-    queue = []
-    dictionary = {start : 0}
+    q = queue.Queue()
+    dictionary = {str(start) : 0}
 
     async def visit(node):
         list_of_neighbours = await get_neighbours(node)
         distance = dictionary.get(node)
+        print("printing list in visit")
+        print(list_of_neighbours)
         for neigh in list_of_neighbours:
-            if dictionary[neigh] <= distance+1:
-                list_of_neighbours.discard(neigh)
-            else:
-                queue.append(neigh)
-                dictionary[neigh] = dictionary+1
+            print("deciding for")
+            print(neigh)
+            if neigh in dictionary.keys() and dictionary[neigh] <= distance+1:
+                list_of_neighbours.remove(neigh)
+            elif distance < 4:
+                print("puttin in queue")
+                q.put(neigh)
+                dictionary[neigh] = distance+1
 
-    queue.append(start)
-    while not queue.empty():
+    q.put(str(start))
+    while not q.empty():
+        print("printing dictionary")
+        print(dictionary)
+        print("printing set")
         tmp = set()
-        while not queue.empty():
-            tmp.add(queue.pop(0))
-        tasks = [visit(node) for node in set]
+        while not q.empty():
+            tmp.add(q.get())
+        print(tmp)
+        tasks = [visit(node) for node in tmp]
         await asyncio.gather(*tasks)
 
+    print("after finding")
+    print(dictionary)
 
     answer = set()
     for key in dictionary:
@@ -82,11 +96,21 @@ async def distance4(start):
 
 
 async def main():
-    x = await climb_degree(8037)
-    print(x)
-    await complete_neighbourhood(8031)
-    y = await climb_degree(8034)
-    print(y)
+    #print("climbing 8037")
+    #x = await climb_degree(8037)
+    #print(x)
+    print("calculating distance4")
+    z = await distance4(8030)
+    print(z)
+    print("complete neigh")
+    await complete_neighbourhood(8034)
+    #print("climbing 8034")
+    #y = await climb_degree(8034)
+    #print(y)
+    print("calculating distance4")
+    q = await distance4(8030)
+    print(q)
+    print("end")
 
 
 if __name__ == '__main__':
