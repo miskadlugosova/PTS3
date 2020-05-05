6ecf6870a3720b91779fe9f30545ce3f11c6c5bb
miskadlugosova <miskadlugosova@gmail.com>
2020-05-05 20:17:43 +0200

    added tests for climb and distance

--- test_coroutines.py
+++ test_coroutines.py
@@ -31,9 +31,9 @@ class TestCompleteNeighbourhood(unittest.TestCase):
 
     @patch('coroutines.get_neighbours', new_callable=GetNeighboursNotNetwork)
     @patch('coroutines.connect', new_callable=ConnectNotNetwork)
-    def test_complete(self, neigh_mock, connect_mock):
+    def test_complete_connects_two_not_connected_neighbours(self, neigh_mock, connect_mock):
         self.assertFalse(('0', '2') in graph)
-        loop = asyncio.get_event_loop()
+        loop = asyncio.new_event_loop()
         loop.run_until_complete(coroutines.complete_neighbourhood('1'))
         loop.close()
         self.assertTrue(('0', '2') in graph)
@@ -43,17 +43,32 @@ class TestCompleteNeighbourhood(unittest.TestCase):
 class TestClimbDegree(unittest.TestCase):
     def setUp(self):
         global graph
-        graph = {(0, 1), (1, 0), (1, 2), (2, 1), (2, 3), (3, 2), (3, 4), (4, 3)}
+        graph = {('0', '1'), ('1', '0'), ('1', '2'), ('2', '1'), ('2', '3'), ('3', '2'), ('3', '4'), ('4', '3')}
+
+    @patch('coroutines.get_neighbours', new_callable=GetNeighboursNotNetwork)
+    @patch('coroutines.connect', new_callable=ConnectNotNetwork)
+    def test_linear_graph_climb_from_edge(self, neigh_mock, connect_mock):
+        loop1 = asyncio.new_event_loop()
+        result = loop1.run_until_complete(coroutines.climb_degree('4'))
+        loop1.close()
+        self.assertEqual(result, '1')
 
 
 # graf z papiera - do vrcholu existuju cesty dlzky 3 aj 4
 class TestDistance4(unittest.TestCase):
     def setUp(self):
         global graph
-        graph = {(0, 1), (1, 0), (0, 2), (2, 0), (0, 3), (3, 0), (1, 4), (4, 1), (4, 6), (6, 4), (2, 5), (5, 2),
-                 (5, 6), (6, 5),
-                 (6, 7), (7, 6)}
+        graph = {('0', '1'), ('1', '0'), ('0', '2'), ('2', '0'), ('0', '3'), ('3', '0'), ('1', '4'), ('4', '1'), ('4', '6'), ('6', '4'), ('2', '5'), ('5', '2'),
+                 ('5', '6'), ('6', '5'),
+                 ('6', '7'), ('7', '6')}
 
+    @patch('coroutines.get_neighbours', new_callable=GetNeighboursNotNetwork)
+    @patch('coroutines.connect', new_callable=ConnectNotNetwork)
+    def test_graph_with_different_paths_to_one_node(self, neigh_mock, connect_mock):
+        loop1 = asyncio.new_event_loop()
+        result = loop1.run_until_complete(coroutines.distance4('0'))
+        loop1.close()
+        self.assertEqual(result, {'7'})
 
 
 if __name__ == '__main__':
